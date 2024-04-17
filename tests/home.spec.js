ea4d6ad38b2af888ca9f92f99ce512d36627b49c
Chandima Ranaweera <chan4lk@gmail.com>
2024-04-17 12:35:17 +0530

    add artillery

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,8 +1,6 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://chan4lk.github.io/');
-  await page.getByRole('link', { name: 'Skills' }).click();
-  await page.getByRole('link', { name: 'Open Source' }).click();
-  await expect(page.getByText("Open Source Projects")).toBeVisible();
-});
+module.exports = { smokeTests };
+ 
+async function smokeTests(page) {
+    await page.goto('https://chan4lk.github.io/');
+    await page.getByRole('link', { name: 'Open Source' }).click();
+}
